48eba1d3b531fb12d6634fe7be91b777e89fbf37
xmls_security <srvroman666@gmail.com>
2026-06-28 15:12:54 +1000

    Add Compose entry activity

---
+++ app/src/main/java/com/travel/paris/ComposeMainActivity.kt
@@ -0,0 +1,21 @@
+package com.travel.paris
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import com.travel.paris.ui.AppRoot
+
+class ComposeMainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface {
+                    AppRoot()
+                }
+            }
+        }
+    }
+}
