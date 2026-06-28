f1bc4776a5c240cc4e52577db7e7222f747587ac
xmls_security <srvroman666@gmail.com>
2026-06-28 15:12:59 +1000

    Add simple Compose root

---
+++ app/src/main/java/com/travel/paris/ui/AppRoot.kt
@@ -0,0 +1,11 @@
+package com.travel.paris.ui
+
+import androidx.compose.runtime.Composable
+import com.travel.paris.ComposeMainActivity
+import com.travel.paris.MapScreen
+import androidx.compose.material3.Text
+
+@Composable
+fun AppRoot() {
+    Text(text = "Travel Paris App Root")
+}
