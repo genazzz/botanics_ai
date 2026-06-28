7ac32d28d8d9fffd6f9ef15cc7a180a52b1e82cf
xmls_security <srvroman666@gmail.com>
2026-06-28 15:11:53 +1000

    Add Map screen placeholder

---
+++ app/src/main/java/com/travel/paris/MapScreen.kt
@@ -0,0 +1,9 @@
+package com.travel.paris
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MapScreen() {
+    Text(text = "Map will show route in Paris")
+}
