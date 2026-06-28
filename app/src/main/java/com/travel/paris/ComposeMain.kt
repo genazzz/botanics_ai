a26417752cc777147357bdef1e001792b45c18b1
xmls_security <srvroman666@gmail.com>
2026-06-28 15:11:05 +1000

    Add Compose UI entry sample

---
+++ app/src/main/java/com/travel/paris/ComposeMain.kt
@@ -0,0 +1,9 @@
+package com.travel.paris
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TravelHomeScreen() {
+    Text(text = "Travel Paris Compose UI")
+}
